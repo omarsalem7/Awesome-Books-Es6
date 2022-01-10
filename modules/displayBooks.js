b87a518508900806c3e644add2016bc852a6173d
Omar <omarsalem0721@gmail.com>
2022-01-10 17:50:15 +0200

    Update changes

--- modules/displayBooks.js
+++ modules/displayBooks.js
@@ -4,11 +4,16 @@ const displayBooks = (booksList) => {
   booksList.list.forEach((book) => {
     booksHtml += `<li class="book-item">
                             <div class="book-info">
-                                <p class="book-title">${book.title.charAt(0).toUpperCase() + book.title.slice(1)}</p>
+                                <p class="book-title">${
+  book.title.charAt(0).toUpperCase()
+                                  + book.title.slice(1)
+}</p>
                                 <p class="book-by">by</p>
                                 <p class="book-author">${book.author}</p>
                             </div>
-                            <button id="${book.id}" class="remove-btn" type="button">Remove</button>
+                            <button id="${
+  book.id
+}" class="remove-btn" type="button">Remove</button>
                        </li>
               `;
   });
@@ -18,17 +23,15 @@ const displayBooks = (booksList) => {
   const removeBtns = document.querySelectorAll('.remove-btn');
   removeBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-      /* eslint-disable */
-            const element = btn.parentNode;
-            booksList.removeBook(parseInt(e.target.id));
-            element.remove();
-        });
+      const element = btn.parentNode;
+      booksList.removeBook(parseInt(e.target.id, 8));
+      element.remove();
     });
+  });
 
-    if (booksList.list.length === 0) {
-        books.innerHTML = '<h4 class="no-books">No books to show</h4>';
-    }
-
-}
+  if (booksList.list.length === 0) {
+    books.innerHTML = '<h4 class="no-books">No books to show</h4>';
+  }
+};
 
 export default displayBooks;
