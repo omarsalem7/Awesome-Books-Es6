166ef742b2e357d8f10689c7506fd7e92554fb03
Omar <omarsalem0721@gmail.com>
2022-01-10 15:26:06 +0200

    Seprate code in modules

---
+++ index.js
@@ -0,0 +1,30 @@
+import displayBooks from "./modules/displayBooks.js";
+import Books from "./modules/books.js";
+import navigation from "./modules/navigation.js";
+import { DateTime } from './node_modules/luxon/build/es6/luxon.js';
+
+const booksList = new Books();
+displayBooks(booksList);
+
+const form = document.querySelector('.add-books');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = Math.floor(Math.random() * 100000) + 1;
+    const title = document.querySelector('.input-title').value.trim();
+    const author = document.querySelector('.input-author').value.trim();
+    const newBook = { id, author, title };
+    if (title && author) {
+        booksList.addBook(newBook);
+        displayBooks(booksList);
+        document.querySelector('.input-title').value = '';
+        document.querySelector('.input-author').value = '';
+    } else {
+        alert('Please enetr title and author');
+    }
+});
+
+// navigation SPA
+navigation();
+
+// Add time
+
---
+++ modules/books.js
@@ -0,0 +1,21 @@
+
+export default class Books {
+    constructor() {
+        this.list = localStorage.getItem('books')
+            ? JSON.parse(localStorage.getItem('books'))
+            : [];
+    }
+
+    addBook(book) {
+        this.list.push(book);
+        localStorage.setItem('books', JSON.stringify(this.list));
+
+    }
+
+    removeBook(bookID) {
+        this.list = this.list.filter((book) => book.id !== bookID);
+        localStorage.setItem('books', JSON.stringify(this.list));
+
+
+    }
+}
